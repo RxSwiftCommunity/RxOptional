5e19cf95f2690af439ef5dcf728e18392009869a
Ivan Bruel <ivan.bruel@gmail.com>
2016-09-29 15:30:08 +0100

    Driver+Occupiable migrated to SharedSequenceConvertibleType

--- Source/Driver+Occupiable.swift
+++ Source/Driver+Occupiable.swift
@@ -1,19 +1,19 @@
 import Foundation
 import RxCocoa
 
-public extension Driver where Element: Occupiable {
+public extension SharedSequenceConvertibleType where SharingStrategy == DriverSharingStrategy, E: Occupiable {
     /**
      Filters out empty elements.
 
      - returns: `Driver` of source `Driver`'s elements, with empty elements filtered out.
      */
     
-    public func filterEmpty() -> Driver<Element> {
-        return self.flatMap { element -> Driver<Element> in
+    public func filterEmpty() -> Driver<E> {
+        return self.flatMap { element -> Driver<E> in
             guard element.isNotEmpty else {
-                return Driver<Element>.empty()
+                return Driver<E>.empty()
             }
-            return Driver<Element>.just(element)
+            return Driver<E>.just(element)
         }
     }
 
@@ -25,12 +25,12 @@ public extension Driver where Element: Occupiable {
      - returns: `Driver` of the source `Driver`'s elements, with empty elements replaced by the handler's returned non-empty elements.
      */
     
-    public func catchOnEmpty(_ handler: @escaping () -> Driver<Element>) -> Driver<Element> {
-        return self.flatMap { element -> Driver<Element> in
+    public func catchOnEmpty(_ handler: @escaping () -> Driver<E>) -> Driver<E> {
+        return self.flatMap { element -> Driver<E> in
             guard element.isNotEmpty else {
                 return handler()
             }
-            return Driver<Element>.just(element)
+            return Driver<E>.just(element)
         }
     }
 }
