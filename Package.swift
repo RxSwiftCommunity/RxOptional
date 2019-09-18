4e0a0a172f42b8635557b5152017073a34f013b4
Behzad Ardehei <behzad.ardeh.e@gmail.com>
2019-09-18 09:32:42 +0430

    added RxSwift in target dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "RxOptional", dependencies: []),
+        .target(name: "RxOptional", dependencies: ["RxSwift"]),
         .testTarget(name: "RxOptionalTests", dependencies: ["RxOptional"]),
     ]
 )
