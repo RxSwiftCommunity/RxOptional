17d454513cb631e625bc995e27b62159fcd35a12
Jaeyun,Oh <goodocy@linecorp.com>
2019-09-25 11:48:33 +0900

    Support SPM ( Swift Package Manager )

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.0
+import PackageDescription
+
+let package = Package(
+    name: "RxOptional",
+    products: [
+        .library(name: "RxOptional", targets: ["RxOptional"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "RxOptional",
+            dependencies: ["RxSwift"],
+            path: "Sources"
+        )
+    ]
+)
