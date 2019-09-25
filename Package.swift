6bb00da62a16266c91b5e28738d59633858935e9
Jaeyun,Oh <goodocy@linecorp.com>
2019-09-25 15:08:45 +0900

    Support Tests with SPM

--- Package.swift
+++ Package.swift
@@ -8,12 +8,19 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.2"))
     ],
     targets: [
         .target(
             name: "RxOptional",
             dependencies: ["RxSwift", "RxCocoa"],
             path: "Sources"
+        ),
+        .testTarget(
+            name: "RxOptionalTests",
+            dependencies: ["RxOptional", "Quick", "Nimble"],
+            path: "Tests"
         )
     ],
     swiftLanguageVersions: [.v5]
