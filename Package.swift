892e083d44e745b09f718fc93ed652be8655d4b3
Behzad Ardehei <behzad.ardeh.e@gmail.com>
2019-09-29 12:03:43 +0330

    add SPM in circleci/config

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RxOptional",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "RxOptional", targets: ["RxOptional"]),
